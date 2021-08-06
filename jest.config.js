8ea6b17b6ad6283b1f802d968416cbf639ee1f94
Jonathan Yu <jyu@palantir.com>
2021-08-06 12:47:38 -0700

    Fix ts-jest presets

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  "preset": "ts-jest/presets/js-with-ts",
 };
